332adf0dc4b06edf63f103498c9aa6c0c66c7754
Steven Ortiz <juansf_456@hotmail.com>
2024-09-19 16:46:10 -0500

    avance redis

---
+++ config/redis.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+
+function getRedisConnection()
+{
+    $client = new Predis\Client([
+        'scheme' => 'tcp',
+        'host' => '127.0.0.1',
+        'port' => 6379,
+    ]);
+
+    return $client;
+}
--- models/usuario.php
+++ models/usuario.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../config/redis.php';
 
 class Usuario
 {
@@ -31,5 +32,42 @@ class Usuario
 
         return $stmt->rowCount();
 
+    }
+
+    public static function saveRedis($nombre, $apellido, $correo, $cedula, $password, $telefono, $direccion, $user_type, $id_estado, $confCorreo, $municipio_id) // Por defecto 1 hora
+    {
+        $redis = getRedisConnection();
+
+        $userId = uniqid('user:', true); //ID unico para el usuario
+
+        //aca se crea un hash con los datos del usuario
+        $userData = [
+            'name' => $nombre,
+            'lastname' => $apellido,
+            'email' => $correo,
+            'cedula' => $cedula,
+            'password' => password_hash($password, PASSWORD_BCRYPT), // Encriptar la contraseña
+            'telefono' => $telefono,
+            'direccion' => $direccion,
+            'user_type' => $user_type,
+            'id_estado' => $id_estado,
+            'confCorreo' => $confCorreo,
+            'municipio_id' => $municipio_id
+        ];
+
+        $expiration = 3600; // equivale a una hora
+        // Acá almacenamos el hash en redis
+        $redis->hmset($userId, $userData);
+
+        $redis->expire($userId, $expiration);
+
+        return $userId;
+
+    }
+
+    public static function getUser($userId)
+    {
+        $redis = getRedisConnection();
+        return $redis->hgetall($userId);
     }
 }

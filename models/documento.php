a03479932fefe4d943519ce1ff50f90636981ca9
Steven Ortiz <juansf_456@hotmail.com>
2024-09-24 16:44:33 -0500

    consulta save documento

--- models/documento.php
+++ models/documento.php
@@ -4,8 +4,29 @@ class Documento
 {
 
 
-    public static function uploadDocument($id_abogado, $tipo_documento, $ruta_archivo)
+    public static function save($id_abogado, $tipo_documento, $ruta_archivo, $fecha_subida)
     {
+        $db = getConnection();
 
+        $sql = "INSERT INTO documentos (id_abogado,tipo_documento,ruta_archivo,fecha_subida)
+        VALUES(:id_abogado,:tipo_documento,:ruta_archivo,:fecha_subida);";
+        $stmt = $db->prepare($sql);
+
+        try {
+            $stmt->execute([
+                ':id_abogado' => $id_abogado,
+                ':tipo_documeto' => $tipo_documento,
+                ':ruta_archivo' => $ruta_archivo,
+                ':fecha_subida' => $fecha_subida
+            ]);
+
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            error_log("error al guardar documentos: " . $e->getMessage());
+            return [
+                'status' => 'error',
+                'message' => 'No se pudo guardar el documento: ' . $e->getMessage() // Mensaje de error
+            ];
+        }
     }
 }
